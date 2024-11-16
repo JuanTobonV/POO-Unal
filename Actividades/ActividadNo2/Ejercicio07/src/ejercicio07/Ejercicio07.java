1b10243ad0ce79d103a209401da4675e8a4415e5
Gustavo Adolfo Pérez Pérez <elcerroticooficial@gmail.com>
2024-11-16 15:12:22 -0500

    fix: no files in the last commit

--- Actividades/ActividadNo2/Ejercicio07/src/ejercicio07/Ejercicio07.java
+++ Actividades/ActividadNo2/Ejercicio07/src/ejercicio07/Ejercicio07.java
@@ -1,20 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package ejercicio07;
+package ejercicio7;
 
-/**
- *
- * @author elcer
- */
-public class Ejercicio07 {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class Ejercicio7 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Crear un objeto Scanner para capturar la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+        
+        // Solicitar los valores de A y B al usuario
+        System.out.print("Ingrese el valor de A: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Ingrese el valor de B: ");
+        int b = scanner.nextInt();
+        
+        // Comparar A con B y mostrar el mensaje correspondiente
+        if (a > b) {
+            System.out.println("A es mayor que B");
+        } else if (a == b) {
+            System.out.println("A es igual a B");
+        } else {
+            System.out.println("A es menor que B");
+        }
     }
-    
 }
